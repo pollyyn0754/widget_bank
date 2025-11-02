2a20d1d39821ce73037869b92fda8ea1a03dcb65
polina dorozhkina <pollyyn0754@gmail.com>
2025-11-02 10:40:18 +0300

    2

--- src/widget.py
+++ src/widget.py
@@ -1,2 +1,3 @@
 def get_date(operation_info: str) -> str:
+    """Функция, которая принимает строку с информацией об операции и возвращает строку с датой в формате ДД.ММ.ГГГГ"""
     return ".".join([operation_info[8:10], operation_info[5:7], operation_info[:4]])
