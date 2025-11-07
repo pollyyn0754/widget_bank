3924b1ebc12f856f923991e2caec4f8ecf94789b
polina dorozhkina <pollyyn0754@gmail.com>
2025-11-07 23:47:41 +0300

    init

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,13 @@
+def mask_account_card(account_card: str) -> str:
+    """Функция, которая примает строку, содержащую тип и номер карты или счета, и
+    возвращает строку с замаскированным номером"""
+    if account_card[-20:].isdigit():
+        return "Счет **" + account_card[-4:]
+    else:
+        return account_card[:-12] + " " + account_card[-12:-10] + "** **** " + account_card[-4:]
+
+
 def get_date(operation_info: str) -> str:
-    """Функция, которая принимает строку с информацией об операции и возвращает строку с датой в формате ДД.ММ.ГГГГ"""
+    """Функция, которая принимает строку с информацией об операции и
+    возвращает строку с датой в формате ДД.ММ.ГГГГ"""
     return ".".join([operation_info[8:10], operation_info[5:7], operation_info[:4]])
